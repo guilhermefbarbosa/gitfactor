5da346e02ed205eb1c49386ab5739a4f0c8f0e79
Guilherme Barbosa <guilherme.fbarbosa@gmail.com>
2014-11-06 08:26:28 -0200

    Two new queries for github api.

--- gitfactor/src/main/java/br/com/guilhermebarbosa/git/GitfactorMain.java
+++ gitfactor/src/main/java/br/com/guilhermebarbosa/git/GitfactorMain.java
@@ -1,11 +1,18 @@
 package br.com.guilhermebarbosa.git;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import br.com.guilhermebarbosa.gitfactor.Constants;
 import br.com.guilhermebarbosa.gitfactor.GitHubAnalyser;
 
 public class GitfactorMain {
+	public static Logger LOGGER = Logger.getLogger(GitfactorMain.class);
+	
 	public static void main(String[] args) {
 		try {
 			String tmpFolder = args[0];
@@ -15,12 +22,34 @@ public class GitfactorMain {
 			// shutdownhook to stop context gracefully
 			classPathXmlApplicationContext.registerShutdownHook();
 			classPathXmlApplicationContext.start();
-			// get service
-			GitHubAnalyser gitHubAnalyser = (GitHubAnalyser) br.com.guilhermebarbosa.git.ApplicationContext.getInstance().getBean("gitHubAnalyser"); 
-			gitHubAnalyser.analyseGitHubByQueryUrl(Constants.GIT_HUB_QUERY_REPOS, tmpFolder, analyse);
+			// obtem a query dependendo do ambiente
+			String query = obterQueryString();
+			if ( query == null ) {
+				LOGGER.error("Error getting query string. Program will terminate.");
+				classPathXmlApplicationContext.stop();
+			} else {
+				LOGGER.info(String.format("Query String: %1$s", query));
+				// get service
+				GitHubAnalyser gitHubAnalyser = (GitHubAnalyser) br.com.guilhermebarbosa.git.ApplicationContext.getInstance().getBean("gitHubAnalyser"); 
+				gitHubAnalyser.analyseGitHubByQueryUrl(query, tmpFolder, analyse);
+			}
+			// close resources
 			classPathXmlApplicationContext.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+			LOGGER.error(e.getMessage(), e);
 		}
+	}
+
+	private static String obterQueryString() throws IOException {
+		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("env.properties");
+		Properties properties = new Properties();
+		properties.load(inputStream);
+		String env = (String) properties.get("env");
+		if ( env.equals("juvenal") ) {
+			return Constants.GIT_HUB_QUERY_REPOS_JUVENAL;
+		} else if ( env.equals("barbosa") ) {
+			return Constants.GIT_HUB_QUERY_REPOS_BARBOSA;
+		}
+		return null;
 	}
 }
